93a6c86df014840ca3a1a9458429bef6a752ce85
rickysoliman <rickysoliman@gmail.com>
2020-10-05 16:56:51 -0700

    Assignment Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  // define element if it is undefined
+  element = element === undefined ? document.body : element;
+
+  // create results array
+  var results = [];
+
+  // if element contains classList and classList contains className
+  if (element.classList && element.classList.contains(className)) {
+    results.push(element);
+  }
+
+  // if element contains child Nodes
+  if (element.childNodes) {
+    _.each(element.childNodes, function(item) {
+      results = results.concat(getElementsByClassName(className, item));
+    });
+  }
+
+  return results;
 };
